dd920294fccbedf8c3d1f7e9914274a23813bbd0
Jess Horalek <jhoralek2@huskers.unl.edu>
2025-08-28 18:07:52 +0000

    Thu, Aug 28, 2025, 1:07 PM -05:00

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jess Horalek
+ *         jhoralek2@huskers.unl.edu
+ * Date: 2025/08/28
+ *
+ * A program to introduce myself and my major in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+printf("Hello World, my name is Jess Horalek!\n");
+printf("My major is Electrical Engineering!\n");
+return 0;
+}
